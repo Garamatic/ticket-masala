a237370e7686c8ddbad68e49a6cba58fc0b4e701
KingTensai <maarten.gortz@student.ehb.be>
2025-10-19 00:37:40 +0200

    Work in progress project setup

--- IT-Project2526/Models/Project.cs
+++ IT-Project2526/Models/Project.cs
@@ -2,11 +2,17 @@
 
 namespace IT_Project2526.Models
 {
-    public class Project
+    public class Project : BaseModel
     {
         public Status Status { get; set; }
         public required string Name { get; set; }
         public required string Description { get; set; }
         public required Employee ProjectManager { get; set; }
+
+        public DateTime? CompletionTarget { get; set; }
+        public DateTime? CompletionDate { get; set; }
+
+        public List<Ticket> Tasks { get; set; } = [];
+        public List<Resource> Resources { get; set; } = [];
     }
 }
---
+++ IT-Project2526/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IT_Project2526.Models
+{
+    public abstract class ApplicationUser : IdentityUser
+    {
+        public required string Name { get; set; }
+        public required string Phone { get; set; }
+    }
+    public class Guest : ApplicationUser { }
+    public  class Employee : ApplicationUser
+    {
+        public required string Team { get; set; }
+        public required EmployeeLevel Level { get; set; }
+    }
+    public class Customer : ApplicationUser
+    {
+        public string? Code { get; set; }
+    }
+}

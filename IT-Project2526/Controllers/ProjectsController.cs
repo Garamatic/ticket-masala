9dd6bd04242d9efbc0fc465a628ce5d59889a78d
Charlotte -Schroer <charlotte.schroer@student.ehb.be>
2025-10-26 16:11:37 +0100

    Create New Project
    Pagina om een nieuw project aan te maken is toegevoegd. De connectie met de database zou nog juist moeten gebeuren

--- IT-Project2526/Controllers/ProjectsController.cs
+++ IT-Project2526/Controllers/ProjectsController.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.Linq;
 using IT_Project2526.Models;
+using IT_Project2526.Models.ViewModels;
 using IT_Project2526;
 using System;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using IT_Project2526.ViewModels;
 
 //Database Connectie moet nog toegevoegd worden
 
@@ -15,62 +15,26 @@ namespace IT_Project2526.Controllers
 {
     public class ProjectsController : Controller
     {
-        private readonly ITProjectDB _context;
-        public ProjectsController(ITProjectDB context)
+        private readonly ApplicationDbContext _context;
+        public ProjectsController(ApplicationDbContext context)
         {
             _context = context;
-            try
-            {
-                if (!_context.Database.CanConnect())
-                {
-                    throw new Exception("Fatal Error: No Database Connection Possible");
-                }
-            }
-            catch (Exception ex) 
-            {
-                Console.WriteLine(ex.Message);
-                throw;
-            }
         }
 
         public IActionResult Index()
         {
-            //Projecten uit Db halen met hun tickets
-            var projectsOfDb = _context.Projects
-                                       .Include(p => p.Tasks)
-                                       .Include(p => p.ProjectManager)
-                                       .ToList();
-
-            //Models naar ViewModels
-            List<ProjectTicketViewModel> viewModels = projectsOfDb.Select(p => new ProjectTicketViewModel
-            {
-                ProjectDetails = new ProjectViewModel
-                {
-                    Name = p.Name,
-                    Description = p.Description,
-                    Status = p.Status,
-                    ProjectManager = p.ProjectManager,
-                },
-                Tasks = p.Tasks.Select(t => new TicketViewModel
-                {
-                    Guid = t.Guid,
-                    TicketStatus = t.TicketStatus,
-                    CreationDate = t.CreationDate,
-                }).ToList()
-            }).ToList();
-
-            return View(viewModels);
+            return View();
         }
 
         [HttpGet]
         public IActionResult NewProject()
         { //Ophalen bestaande klanten en voorbereiden van de new project form
-            var existingCustomers = _context.Customers.ToList();
+            var existingCustomers = _context.Customer.ToList();
             var viewModel = new NewProject
             {
                 CustomerList = existingCustomers.Select(c => new SelectListItem
                 {
-                    Value = c.Id.ToString(),
+                    Value = c.Guid.ToString(),
                     Text = c.Name
                 }).ToList(),
                 IsNewCustomer = true
@@ -103,7 +67,7 @@ namespace IT_Project2526.Controllers
                 else
                 {
                     // Zoek de bestaande klant op basis van de SelectedCustomerId
-                    projectCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.Id == viewModel.SelectedCustomerId);
+                    projectCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.Guid == viewModel.SelectedCustomerId);
 
                     if (projectCustomer == null)
                     {
@@ -112,7 +76,7 @@ namespace IT_Project2526.Controllers
                         // Herlaad de klantenlijst en retourneer de view om de fout te tonen
                         viewModel.CustomerList = _context.Customers.ToList().Select(c => new SelectListItem
                         {
-                            Value = c.Id.ToString(),
+                            Value = c.Guid.ToString(),
                             Text = c.Name
                         }).ToList();
                         return View(viewModel);
@@ -124,7 +88,7 @@ namespace IT_Project2526.Controllers
                 {
                     Name = viewModel.Name,
                     Description = viewModel.Description,
-                    Status = Status.Pending, // Stel de status in op basis van je logica
+                    Status = Status.Open, // Stel de status in op basis van je logica
                     // ProjectManager moet ook nog worden ingevuld, bijvoorbeeld met de ingelogde gebruiker
                     ProjectManager = null, // Dit moet je nog implementeren
                     // ... andere Project eigenschappen
@@ -146,7 +110,7 @@ namespace IT_Project2526.Controllers
             // Als de validatie faalt, herlaad dan de klantenlijst en toon het formulier opnieuw
             viewModel.CustomerList = _context.Customers.ToList().Select(c => new SelectListItem
             {
-                Value = c.Id.ToString(),
+                Value = c.Guid.ToString(),
                 Text = c.Name
             }).ToList();
             return View(viewModel);

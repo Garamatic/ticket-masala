74a19e72c5d1065b8b62c59d32ba4c95e999eb2d
Charlotte -Schroer <charlotte.schroer@student.ehb.be>
2025-10-26 16:11:37 +0100

    Create New Project
    Pagina om een nieuw project aan te maken is toegevoegd. De connectie met de database zou nog juist moeten gebeuren

---
+++ IT-Project2526/Models/ViewModels/NewProjectViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace IT_Project2526.Models.ViewModels
+{
+    public class NewProject
+    {
+        // Projectvelden
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        // De projectmanager wordt meestal ingelogd (of uit een lijst gehaald)
+        // en hoeft hier niet in de ViewModel te staan, tenzij de user deze moet kunnen kiezen.
+
+        // Klantenkeuze
+        public bool IsNewCustomer { get; set; } = true; // Standaard staat 'Nieuwe klant' geselecteerd
+        public Guid? SelectedCustomerId { get; set; }
+        public List<SelectListItem> CustomerList { get; set; }
+
+        // Velden voor een nieuwe klant
+        public string? NewCustomerName { get; set; }
+        public string? NewCustomerEmail { get; set; }
+        public string? NewCustomerPhone { get; set; }
+    }
+}

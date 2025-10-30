ac8aa0c75f404f25f018aa993968f10bc37bc037
Wito De Schrijver <wito.de.schrijver@student.ehb.be>
2025-10-30 15:00:52 +0100

    applicationusermanager and controller

--- IT-Project2526/ITProjectDB.cs
+++ IT-Project2526/ITProjectDB.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data.Common;
 using IT_Project2526.Models;
+using IT_Project2526.Utilities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,31 @@ namespace IT_Project2526
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            //roles
+            var admin = new IdentityRole
+            {
+                Id = "1",
+                Name = Constants.RoleAdmin,
+                NormalizedName = Constants.RoleAdmin.ToUpper()
+            };
+
+            var employee = new IdentityRole
+            {
+                Id = "2",
+                Name = Constants.RoleEmployee,
+                NormalizedName = Constants.RoleEmployee.ToUpper()
+            };
+
+            var cust = new IdentityRole
+            {
+                Id = "3",
+                Name = Constants.RoleCustomer,
+                NormalizedName = Constants.RoleCustomer.ToUpper()
+            };
+
+            modelBuilder.Entity<IdentityRole>()
+                .HasData(employee, admin, cust);
 
         }
 

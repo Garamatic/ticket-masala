54eb778841a9f4846a1173feacdf0d5f41be1a51
Juan Benjumea <benjumeamoreno@gmail.com>
2026-04-27 18:41:08 +0200

    refactor: add Domain.Repositories and Domain.Services to GlobalUsings
    Add global usings for the new Domain-layer repository and service
    interfaces to reduce boilerplate across the Web project.

--- src/TicketMasala.Web/GlobalUsings.cs
+++ src/TicketMasala.Web/GlobalUsings.cs
@@ -2,3 +2,5 @@
 global using TicketMasala.Domain.Common;
 global using TicketMasala.Domain.Data;
 global using TicketMasala.Domain.Entities;
+global using TicketMasala.Domain.Repositories;
+global using TicketMasala.Domain.Services;
